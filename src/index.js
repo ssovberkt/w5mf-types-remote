ac7331281a986e223e6f839757294e48a608d7eb
Stanislav Okunev <stanislav.okunev@softnet.by>
2022-12-08 19:25:14 +0300

    0.2.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+require('dotenv').config();
+
+if (!argv?.NODE_ENV) {
+  throw new Error('Set NODE_ENV params (development or production)');
+}
+
+if (!['development', 'production'].includes(argv.NODE_ENV)) {
+  throw new Error(`Set NODE_ENV params (development or production). Current: ${argv.NODE_ENV}`);
+}
+
+process.env.NODE_ENV = argv.NODE_ENV
+
+const rimraf = require('rimraf');
 const fs = require('fs');
+const path = require('path');
 const tar = require('tar');
 const axios = require('axios');
 const https = require('https');
+
+const ROOT_DIR = process.cwd();
+
+const config = require(path.resolve(ROOT_DIR, 'src/config'));
+
+const INSTALL_DIR = path.resolve(ROOT_DIR, '@types');
+const TYPES_FILE = 'w5mf-types.tar';
+const UPLOAD_TYPES_FILE = path.resolve(ROOT_DIR, `${INSTALL_DIR}/${TYPES_FILE}`);
+const INSTALL_FILE = path.resolve(ROOT_DIR, TYPES_FILE);
+const REMOTES = config.remotes || {};
 
 const parseRemoteUrl = (remoteUrl) => {
   const parse = remoteUrl.split('@');
@@ -10,32 +35,49 @@ const parseRemoteUrl = (remoteUrl) => {
   return { name: parse[0], url }
 }
 
-module.exports = class W5MFTypesRemotePlugin {
-  constructor(options) {
-    this.options = options; // installDir, archiveFile, remotes
+const run = async (remote) => {
+  const module = parseRemoteUrl(remote);
+
+  try {
+    const response = await axios.get(`${module.url}/${TYPES_FILE}`, {
+      responseType: 'blob',
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    });
+
+    fs.writeFile(UPLOAD_TYPES_FILE, response.data, (error) => {
+      if (error) {
+        console.log(error);
+        throw new Error(`[REMOTE_TYPES] Write file error`);
+      }
+
+      tar.extract({ file: UPLOAD_TYPES_FILE, cwd: INSTALL_DIR }, () => {
+        rimraf(UPLOAD_TYPES_FILE, () => console.log(`[REMOTE_TYPES] Remove '${INSTALL_FILE}' for '${module.url}'`));
+      });
+    });
+
+  } catch (error) {
+    console.log('[REMOTE_TYPES]', error);
+    throw error;
   }
+}
 
-  apply(compiler) {
-    const INSTALL_DIR = this.options?.installDir || '.';
-    const ARCHIVE_FILE = this.options?.archiveFile || 'w5mf-types.tar';
-    const REMOTES = this.options?.remotes || {};
-    const INSTALL_FILE = `${INSTALL_DIR}/${ARCHIVE_FILE}`;
+(() => {
+  rimraf(INSTALL_DIR, async (error) => {
+    if (error) {
+      console.log(`[REMOTE_TYPES] Remove '${INSTALL_DIR}'`, error);
+    }
 
-    compiler.hooks.afterEnvironment.tap("W5MFTypesRemote", async (compilation) => {
+    fs.mkdir(INSTALL_DIR, async (error) => {
+      if (error) {
+        console.log(error);
+        throw new Error(`[REMOTE_TYPES] Mkdir '${INSTALL_DIR}' error`);
+      }
+
       const remotes = Object.values(REMOTES);
       for (let i in remotes) {
-        const app = parseRemoteUrl(remotes[i]);
-        try {
-          const response = await axios.get(`${app.url}/${ARCHIVE_FILE}`, {
-            responseType: 'blob',
-            httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-          });
-          fs.writeFileSync(INSTALL_FILE, response.data);
-          await tar.extract({ file: INSTALL_FILE });
-        } catch (error) {
-          console.log('[W5MF-TYPES-REMOTE][ERROR]', error);
-        }
+        await run(remotes[i]);
       }
     });
-  }
-};
+  });
+
+})();
